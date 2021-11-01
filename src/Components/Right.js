0dccbd17ca1a3097cb7aee1d75565a764e483bf6
Abhishek Verma <avishekv444@gmail.com>
2021-11-01 23:55:01 +0530

    right side completed

--- src/Components/Right.js
+++ src/Components/Right.js
@@ -2,7 +2,21 @@
 
 function Right(){
     return(
-        <h1>right</h1>
+        <div>
+            <div className="mobile">
+                <div className="leftside">
+                    <img src=" https://cdn-icons.flaticon.com/png/512/15/premium/15640.png?token=exp=1635780973~hmac=0437a996ad0f604f8dc3170f9952b02b " />
+                </div> 
+                <div className="middle">
+                    <img src="https://cdn-icons.flaticon.com/png/512/3899/premium/3899618.png?token=exp=1635790794~hmac=ab5a9976ec03a616afebb7119bb5e38a" />
+                    <h5>@anshumanxda</h5>
+                    <h6>Powered By <img  src="https://cdn-icons.flaticon.com/png/512/15/premium/15640.png?token=exp=1635780973~hmac=0437a996ad0f604f8dc3170f9952b02b" />Snipfeed</h6>                                
+                </div>           
+            </div>
+            <div className="rightSide">
+                    <img src="https://cdn-icons.flaticon.com/png/512/2437/premium/2437630.png?token=exp=1635791653~hmac=7b3fa75a5fecefb097128e1a43ffa2db" />
+            </div>    
+        </div>
     )
 }
 
