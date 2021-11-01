9636e84422c2197ae6219438eef99a2ef038ebe6
Abhishek Verma <avishekv444@gmail.com>
2021-11-01 21:07:18 +0530

    aside section completed

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,17 @@
 import Aside from './Aside';
 import Main from './Main';
 
-import {Container,Row,Col} from "react-bootstrap";
+
 
 function App() {
   return (
-    <div>
-        <Container className="noPadding">
-          <Row className="noMargin">
-            <Col className="noPadding"  md={2}> < Aside/> </Col>
-            <Col className="noPadding" md={10}> <Main/> </Col>
-          </Row>
-        </Container>
+    <div className="f">
+        <div className="asideSection">
+            <Aside/>
+        </div>
+        <div className="mainSection">
+            <Main/>
+        </div>
     </div>
   );
 }
--- src/Components/Aside.js
+++ src/Components/Aside.js
@@ -3,17 +3,60 @@ import {Stack} from "react-bootstrap";
 function Aside(){
     return(
         <div className="aside">
-            <Stack gap={1}>
-                <div className="asideIcon border">
+            <Stack gap={3}>
+                <div className="asideIcon logo ">
+                    <div>
+                        <img src="https://cdn-icons.flaticon.com/png/512/15/premium/15640.png?token=exp=1635780973~hmac=0437a996ad0f604f8dc3170f9952b02b" />
+                    </div>
+                </div>
+                <div className="asideIcon b s">
                     <div>
                         <img src="https://cdn-icons-png.flaticon.com/512/996/996334.png" />
                     </div>
                     <div>
-                        <p> My Page </p>
+                        <p className="text"> My Page </p>
                     </div>
                 </div>
-                <div className="bg-light border">Second item</div>
-                <div className="bg-light border">Third item</div>
+                <div className="asideIcon">
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/1319/1319861.png" />
+                    </div>
+                    <div>
+                        <p className="text"> Monetize </p>
+                    </div>
+                </div>
+                <div className="asideIcon">
+                    <div>
+                        <img src="https://cdn-icons.flaticon.com/png/512/2096/premium/2096983.png?token=exp=1635770373~hmac=a861e99922b562f92f8e24725907fc95" />
+                    </div>
+                    <div>
+                        <p className="text"> Analytics </p>
+                    </div>
+                </div>
+                <div className="asideIcon">
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3135/3135673.png" />
+                    </div>
+                    <div>
+                        <p className="text"> Earnings </p>
+                    </div>
+                </div>
+                <div className="asideIcon">
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/809/809522.png" />
+                    </div>
+                    <div>
+                        <p className="text"> Refferals </p>
+                    </div>
+                </div>
+                <div className="asideIcon">
+                    <div>
+                        <img src="https://cdn-icons-png.flaticon.com/512/900/900797.png" />
+                    </div>
+                    <div>
+                        <p className="text"> Settings </p>
+                    </div>
+                </div>
             </Stack>
         </div>
     )
